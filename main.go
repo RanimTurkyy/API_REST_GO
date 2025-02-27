fb09176462f0af862feaaea388207286bf54625e
ihab souey <ihab.souey@gmail.com>
2025-02-27 11:01:49 +0100

    Exercice 3

--- main.go
+++ main.go
@@ -15,16 +15,19 @@ var tasks = []Task{}
 
 func main() {
   r := gin.Default()
+
   r.GET("/ping", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "message": "pong",
     })
   })
+
   r.GET("/tasks", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "tasks": tasks,
     })
   })
+  
   r.POST("/tasks", func(c *gin.Context) {
 		var newTask Task
 		if err := c.BindJSON(&newTask); err != nil {
@@ -33,5 +36,42 @@ func main() {
 		tasks = append(tasks, newTask)
 		c.IndentedJSON(http.StatusCreated, newTask)
 	})
+
+  // Modifier une tâche
+	r.PUT("/tasks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var updatedTask Task
+
+		if err := c.BindJSON(&updatedTask); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Données invalides"})
+			return
+		}
+
+		task, index := getTaskByID(id)
+		if task == nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Tâche non trouvée"})
+			return
+		}
+
+		// Mise à jour de la tâche
+		tasks[index].Title = updatedTask.Title
+		c.JSON(http.StatusOK, tasks[index])
+	})
+
+	// Supprimer une tâche
+	r.DELETE("/tasks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		_, index := getTaskByID(id)
+		if index == -1 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Tâche non trouvée"})
+			return
+		}
+
+		// Suppression de la tâche
+		tasks = append(tasks[:index], tasks[index+1:]...)
+		c.JSON(http.StatusNoContent, nil)
+	})
+
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
